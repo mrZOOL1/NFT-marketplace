2481e7ea656c2da094f84b7b7e162a15d53c27b4
mrZOOL1 <zoolilan8@gmail.com>
2023-11-01 08:47:42 +0200

    capitalized a component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/ThemeProvider"
 import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
 
